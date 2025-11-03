746f047aee48fff0011b1629fa41918b35eca562
mah00j <mah06.hajj@gmail.com>
2025-11-03 12:23:53 +0200

    first commit

---
+++ app/auth/login.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {};
+}
+
+export default Login;
