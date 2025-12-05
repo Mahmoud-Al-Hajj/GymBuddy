fa5fc4a1c0c7664e26e4630321f17ee00fb2e5f2
mah00j <mah06.hajj@gmail.com>
2025-12-06 00:05:30 +0200

    feat: update rate limiter message and remove unused auth middleware

--- src/middleware/RateLimitter.js
+++ src/middleware/RateLimitter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 100, //each IP to 100 requests
   message: {
     success: false,
     error: "Too many requests from this IP, please try again later.",
